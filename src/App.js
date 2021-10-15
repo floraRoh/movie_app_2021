6f097b793d69660e2271ebf36adbcc3deabda638
nohye12 <nohye12@gmail.com>
2021-10-15 21:04:18 +0900

    #2.1 [전 커밋이 #2.0]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import Potato from "./Potato";
+
+// function Food(props) {
+function Food({fav}) { // props object 내부에 fav가 있다
+  // console.log(props);
+  // console.log(props.fav);
+  console.log(fav);
+  return <h3>I love {fav}</h3>;
+}
 
 function App() {
   return (
     <>
-    <div className="App">Hello!!!</div>
-    <Potato />
+      <div className="App">Hello!!!</div>
+      <Food fav="kimchi" somethig={true} papap={["hello", 1, 2, 3, 4, true]} />
+      <Food fav="ramen " />
+      <Food fav="water" />
+      <Food fav="bread" />
+
     </>
   );
 }
