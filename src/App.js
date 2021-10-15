3d75bb94b0ef2e2a0e2d71a9b93bdce5081e6f9e
nohye12 <nohye12@gmail.com>
2021-10-16 04:25:45 +0900

    #3.0

--- src/App.js
+++ src/App.js
@@ -1,77 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Food({ name, picture, rating }) {
-  console.log(name);
-  return (
-    <>
-      <h3>I love {name}</h3>
-      <h4>{rating} / 5.0</h4>
-      <img src={picture} alt={name} />
-    </>
-  );
-}
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.8,
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 5.5,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.7,
-  },
-];
 /**
- * propTypes를 체크해주는 부분
+ * class component는 return이 없다 (function이 아니니까)
+ * class component는 render method를 가지고 있고 나의 app component안에 있다. 왜냐면 내가 react component에서 확장했기 때문
+ *
+ *
+ * function component는 function이고 뭔가를 return하고 screen에 표시된다.
+ * class component는 class이고 react component로 부터 확장되고 screen에 표시된다.
+ *
+ * react는 자동적으로 class component의 render method를 실행한다.
  */
-Food.propTypes= { // 대소문자 & 오타 주의
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-}
-function renderFood(dish) {
-  console.dir(dish);
-  return (
-    <>
-      <Food
-        key={dish.id}
-        name={dish.name}
-        picture={dish.image}
-        rating={dish.rating}
-      />
-    </>
-  );
-}
-
-function App() {
-  return <>{foodILike.map(renderFood)}</>;
+class App extends React.Component {
+  state = {
+    // object
+    // component의 data를 넣을 공간이 있고 이 데이터는 변한다.
+    count: 0,
+  };
+  add = () => {
+    console.log("add");
+  };
+  miuns = () => {
+    console.log("minus");
+  };
+  render() {
+    return (
+      <>
+        <h1>Im a class component : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.miuns}>Minus</button>
+      </>
+    );
+  }
 }
 
 export default App;
