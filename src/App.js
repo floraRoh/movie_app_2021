eb68b7c982ff96531f08a94a104193f31b529610
nohye12 <nohye12@gmail.com>
2021-10-16 05:30:35 +0900

    #4.0 Fetching Movies from API
    * axios
    * async & await

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movies: []
+    movies: [],
   };
-  componentDidMount() {
+
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
+  async componentDidMount() {
     setTimeout(() => {
       this.setState({ isLoading: false });
     }, 6000);
+    /**
+     * fetch()
+     * fetch가 있지만 axios가 더 좋다한다
+     * 
+     * application은 render한다 
+     */
+    // const movies =axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.getMovies();
   }
   render() {
-    /**
-     * 문. 처음에 render하면 호출되는 life cycle method는 ?
-     * 답. componentDidMount
-     */
     const { isLoading } = this.state;
     return (
       <>
