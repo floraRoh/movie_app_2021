8fc4045eba364ecaf29b760b0921485843059d45
nohye12 <nohye12@gmail.com>
2021-10-16 14:22:04 +0900

    #6.1 Building the Router ★

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
 function App() {
   return (
-    <span>hyewon</span>
-  )
+    <HashRouter>
+      {/* 
+      <Route path="/" component={Home}></Route>
+      <Route path="/about"  component={About}></Route> 
+      이렇게 되면 about에도 / 가 있으니까 about 페이지에서는 2개 다 렌더링 된다. 
+      <Route path="/" exact={true} component={Home}>asdgs</Route>
+      exact={true} 를 넣어줘서 path가 정확히 '/' <- 이거 하나일 때만 home을 렌더링 해주게 할 수 있다.
+      */}
+      <Route path="/home">
+        <h1>Home</h1>
+      </Route>
+      <Route path="/home/introduction">
+        <h1>Introduction</h1>
+      </Route>
+      <Route path="/about">
+        <h1>About</h1>
+      </Route>
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function About() {
+  return(
+    <h1>About this page : because... I think... must have to study!</h1>
+  )
+}
+
+export default About;
